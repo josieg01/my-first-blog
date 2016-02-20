6beead025646b060c72e21348d73547d08df12d6
Your Name <you@example.com>
2016-02-20 15:05:42 +0000

    My Django Girls app, first commit

---
+++ intro_python.py
@@ -0,0 +1,13 @@
+def hi(name):
+	print('Hi ' + name + "!")
+girls = ["Jo", 'Hannah', 'Corinne', "Jodie", "You"]
+for name in girls:
+	hi(name)
+	print("Next girl")
+def yo(nickname):
+	print("Yo " + nickname + "!")
+boys = ["Ed", "Dup"]
+for nickname in boys:
+	yo(nickname)
+	print("Next boy")
+	
